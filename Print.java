7f12a1aadd1229e7683f81bca926acd5fb4c116e
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-07-15 12:58:54 +0530

    Difference between print and println

---
+++ Print.java
@@ -0,0 +1,9 @@
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("hello"); // write the sentence move to next line
+        System.out.println("Cutie!");
+        System.out.print("Hello "); // print the same line
+        System.out.print("Cutie!");
+    }
+    
+}
