8deaf20ddb96dfbf87841f202e69802e3d710720
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-07-17 13:22:11 +0530

    Object to class

---
+++ Course.java
@@ -0,0 +1,21 @@
+public class Course {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Oop oo = new Oop();
+		oo.id = 123;
+		oo.name = "full-stack";
+		oo.menter = "siruu";
+		oo.timings = "Depended";
+		oo.duration = 6;
+		
+		oo.start();
+		oo.learn();
+		oo.end();
+	
+
+	}
+
+}
+    
+
---
+++ Oop.java
@@ -0,0 +1,20 @@
+public class Oop {
+	int id ;
+	String name;
+	String menter;
+	String timings;
+	int duration;
+	
+	public void start() {
+		System.out.println(name +"  course started in the june..!");
+		}
+	public void learn() {
+		System.out.println("Focus on Learning and Practice more...!");
+		
+	}
+	public void end() {
+		System.out.println(name+" It is ended on After "+ duration +" months..");
+	}
+	
+
+}
