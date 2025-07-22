13e9f043a779ad2a1997ed8c12dbf3e228caaeaf
amulyanelakuri <amulyanelakuri19@gmail.com>
2025-07-22 15:10:03 +0530

    Constructor,static and non-static methods and variables in Java

---
+++ JavaProgramFlow.java
@@ -0,0 +1,46 @@
+public class JavaProgramFlow {
+	//static variable
+	static int var;
+	//Non- static variable
+	int instaVar;
+	//static block
+	static {
+		System.out.println("Static block will be executed....");
+		var = 34;
+		System.out.println("Static variable is " + var);
+	//Non-static block
+	}
+	{
+		instaVar = 23;
+		System.out.println("Non-static block will be executed...");
+		System.out.println("static Variable:" + var);
+		System.out.println("Instance Variable:" + instaVar);
+	}
+//Constructor
+	public JavaProgramFlow() {
+		super();
+		System.out.println("Constructor will be executed...");
+		var = 98;
+		instaVar = 34;
+		
+	}
+
+     //static method
+	public static void setVar() {
+	
+		System.out.println("Static method will be exectued: " + var);
+	}
+    //non static method
+	public void setInstaVar() {
+		this.instaVar = instaVar;
+		System.out.println("Instance method will be executed:" + instaVar);
+	}
+	
+	public static void main(String[] args) {
+		
+		JavaProgramFlow jp = new JavaProgramFlow();
+		jp.setVar();
+		jp.setInstaVar();
+		
+	}
+}
